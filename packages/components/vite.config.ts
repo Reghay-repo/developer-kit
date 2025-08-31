d1306b4b9e01233581cd6e0484a29efcfa9c0d5c
osmrgh <oussama.reghay.dev@gmail.com>
2025-08-31 21:02:51 +0100

    fix(build): ensure reliable type declaration generation with vite-plugin-dts

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite';
-
+import {defineConfig} from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-    build:{
-        lib:{
-             entry: 'src/index.ts',
-             name: 'DeveloperAcceleratorKit',
-              fileName: (format) => `dev-accelerator-kit.${format}.js`,
+    plugins: [
+      dts()
+    ],
+    build: {
+        lib: {
+            entry: 'src/index.ts',
+            name: 'C4Components',// A name for the UMD build (for use in browsers via <script> tag)
+            fileName: (format) => `c4-components.${format}.js`,
+        },
+        rollupOptions: {
+            // We'll add something here later if we had external dependencies like React
         }
     }
 })
